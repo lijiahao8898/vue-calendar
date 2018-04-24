d3f2244daff9ef0e6ddc93f5ecb4faef92dbaeea
lijiahao8898 <472239548@qq.com>
2018-04-24 16:33:53 +0800

    update

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,7 +1,8 @@
 <template>
-    <div id="calendar" class="calendar">
+    <div id="calendar" class="calendar" ref="calendar">
+        <div id="weather"></div>
         <div class="calendar-title">
-            <p><span class="pre" @click="goPre">pre</span>{{year}} - {{month + 1}}<span class="next" @click="goNext">next</span>
+            <p><span class="pre" @click="goPre">&lt;</span>{{year}} - {{monthText}}<span class="next" @click="goNext">&gt;</span>
             </p>
         </div>
         <div class="calendar-content">
@@ -24,8 +25,9 @@
         data () {
             return {
                 dateTime: new Date(),
-                today: '',
-                toMonth: '',
+                today: '',                  // 当天
+                toMonth: '',                // 当天的月份
+                toYear: '',                 // 当天的年份
                 dateArr: [],
                 weeks: ['一', '二', '三', '四', '五', '六', '日']
             };
@@ -37,19 +39,28 @@
             month () {
                 return this.dateTime.getMonth();
             },
+            monthText () {
+                return (this.month + 1).toString().length < 2 ? '0' + (this.month + 1) : this.month + 1;
+            },
             date () {
                 return this.dateTime.getDate();
             }
         },
         created () {
-            this.$nextTick(() => {
+            this.$nextTick(async () => {
                 const year = this.year;
                 const month = this.month;
                 const date = this.date;
                 this.today = new Date(year, month, date);
                 this.toMonth = month;
-                this.init();
+                this.toYear = year;
+                await this.init();
+                this.findWeather();
+                console.log(this.$refs.calendar);
+                this.$refs.calendar.style.marginTop = (window.innerHeight - this.$refs.calendar.clientHeight) / 2 + 'px';
             });
+        },
+        mounted () {
         },
         methods: {
             init () {
@@ -76,24 +87,52 @@
                     }
                 }
                 this.dateArr = arr;
-                console.log(this.month, this.dateArr);
             },
             goPre () {
                 this.dateTime = new Date(this.dateTime.getFullYear(), this.month - 1, 1);
                 this.init();
-                console.log(this.month);
             },
             goNext () {
                 this.dateTime = new Date(this.dateTime.getFullYear(), this.month + 1, 1);
                 this.init();
-                console.log(this.month);
             },
             isActive (d, today) {
-                return d.getDate() === today.getDate() && this.month === this.toMonth? true : false;
+                return d.getDate() === today.getDate() && this.month === this.toMonth && this.year === this.toYear ? true : false;
             },
             getDayCounts () {
                 let d = this.dateTime;
                 return new Date(d.getFullYear(), (d.getMonth() + 1), 0).getDate();
+            },
+            findWeather () {
+                const cityUrl = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js';
+                console.log(cityUrl);
+                this.$axios.get(cityUrl)
+                    .then(function (response) {
+                    })
+                    .catch(function (error) {
+                    });
+
+//                $.getScript(cityUrl, function (script, textStatus, jqXHR) {
+//                    var citytq = remote_ip_info.city;// 获取城市
+//                    var url = 'http://php.weather.sina.com.cn/iframe/index/w_cl.php?code=js&city=' + citytq + '&day=0&dfc=3';
+//                    $.ajax({
+//                        url: url,
+//                        dataType: 'script',
+//                        scriptCharset: 'gbk',
+//                        success: function (data) {
+//                            var _w = window.SWther.w[citytq][0];
+//                            var _f = _w.f1 + '_0.png';
+//                            if (new Date().getHours() > 17) {
+//                                _f = _w.f2 + '_1.png';
+//                            }
+//                            var img = '<img width=\'16px\' height=\'16px\' src=\'http://i2.sinaimg.cn/dy/main/weather/weatherplugin/wthIco/20_20/' + _f
+//                                + '\' />';
+//                            var tq = citytq + ' ' + img + ' ' + _w.s1 + ' ' + _w.t1 + '℃～' + _w.t2 + '℃ ' + _w.d1 + _w.p1 + '级';
+//
+//                            $('#weather').html(tq);
+//                        }
+//                    });
+//                });
             }
         }
     };
@@ -112,10 +151,18 @@
             .pre {
                 display: inline-block;
                 float: left;
+                cursor: pointer;
+                margin-left: -10px;
+                width: 50px;
+                font-weight: bold;
             }
             .next {
                 display: inline-block;
                 float: right;
+                cursor: pointer;
+                margin-right: -10px;
+                width: 50px;
+                font-weight: bold;
             }
         }
         &-content {
