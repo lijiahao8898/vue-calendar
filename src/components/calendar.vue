0c8a8a464e70f2584d0f18c02b380744c00c2f99
lijiahao8898 <472239548@qq.com>
2018-04-24 18:02:56 +0800

    update

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -55,8 +55,6 @@
                 this.toMonth = month;
                 this.toYear = year;
                 await this.init();
-                this.findWeather();
-                console.log(this.$refs.calendar);
                 this.$refs.calendar.style.marginTop = (window.innerHeight - this.$refs.calendar.clientHeight) / 2 + 'px';
             });
         },
@@ -102,37 +100,6 @@
             getDayCounts () {
                 let d = this.dateTime;
                 return new Date(d.getFullYear(), (d.getMonth() + 1), 0).getDate();
-            },
-            findWeather () {
-                const cityUrl = 'https://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js';
-                console.log(cityUrl);
-                this.$axios.get(cityUrl)
-                    .then(function (response) {
-                    })
-                    .catch(function (error) {
-                    });
-
-//                $.getScript(cityUrl, function (script, textStatus, jqXHR) {
-//                    var citytq = remote_ip_info.city;// 获取城市
-//                    var url = 'http://php.weather.sina.com.cn/iframe/index/w_cl.php?code=js&city=' + citytq + '&day=0&dfc=3';
-//                    $.ajax({
-//                        url: url,
-//                        dataType: 'script',
-//                        scriptCharset: 'gbk',
-//                        success: function (data) {
-//                            var _w = window.SWther.w[citytq][0];
-//                            var _f = _w.f1 + '_0.png';
-//                            if (new Date().getHours() > 17) {
-//                                _f = _w.f2 + '_1.png';
-//                            }
-//                            var img = '<img width=\'16px\' height=\'16px\' src=\'http://i2.sinaimg.cn/dy/main/weather/weatherplugin/wthIco/20_20/' + _f
-//                                + '\' />';
-//                            var tq = citytq + ' ' + img + ' ' + _w.s1 + ' ' + _w.t1 + '℃～' + _w.t2 + '℃ ' + _w.d1 + _w.p1 + '级';
-//
-//                            $('#weather').html(tq);
-//                        }
-//                    });
-//                });
             }
         }
     };
